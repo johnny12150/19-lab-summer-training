fc4e6c61cc6964a120431cd1210a433c1cdb7dff
wade <johnny12150@gmail.com>
2019-08-20 10:26:23 +0800

    support larger transformation from text to df

--- 0819/read_json.py
+++ 0819/read_json.py
@@ -1,3 +1,12 @@
 import pandas as pd
+import json
+from pandas.io.json import json_normalize
 
-df = pd.read_json('./data.json')
+df = pd.read_json('./data_100000.json')
+print("RAM用量: "+ str(df.memory_usage(index=True).sum()))
+print(df.info())
+
+# data_str = open('data_100000.json', "r",encoding="utf-8").read()
+# data_list = json.loads(data_str)
+# df2 = json_normalize(data_list)
+# print(df2.info())
--- 0819/to_josn.py
+++ 0819/to_josn.py
@@ -1,16 +1,17 @@
 import json
 count = 0
-n = 10
+path = "C:\\Users\\Wade\\Downloads\\dblp.v11\\dblp_papers_v11.txt"
+n = 100000
 result = []
 # 讀取原始dataset
-with open("C:\\Users\\Wade\\Downloads\\dblp.v11\\dblp_papers_v11.txt", 'r') as f:
+with open(path, 'r') as f:
     # 根據 \n分批讀取，避免ram大爆炸 (全部一次讀入加上轉成json需要大概 45G RAM)
     for line in f:
         count += 1
         lines = json.loads(line)
         result.append(lines)
         # 決定想要轉幾筆資料存到json檔
-        if n >= 10:
-            with open('./data.json', 'w', encoding='utf-8') as f2:
-                json.dump(result, f2, ensure_ascii=False, indent=4)
+        if count >= n:
+            with open('./data_' + str(n)+'.json', 'w', encoding='utf-8') as f2:
+                json.dump(result, f2, indent=4)
             break
